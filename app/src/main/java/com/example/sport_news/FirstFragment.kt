d1fe9c809ce1b3ea25c1805da2b4b5bd5abc2032
akki_bhai <85338101+chandan349@users.noreply.github.com>
2022-03-11 12:07:22 +0530

    initial commit

---
+++ app/src/main/java/com/example/sport_news/FirstFragment.kt
@@ -0,0 +1,53 @@
+package com.example.sport_news
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.sport_news.Models.Sport
+import com.example.sport_news.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    private  fun get_sport_list(): List<Sport>{
+        return listOf(
+            Sport("BasketBall",R.drawable.image1, summary = "Basketball star Brittney Griner is the latest American to be detained in Russia as supporters work desperately to free them"),
+            Sport("Cricket",R.drawable.image2, summary = "Basketball star Brittney Griner is the latest American to be detained in Russia as supporters work desperately to free them"),
+            Sport("Badminton",R.drawable.image3, summary = "Basketball star Brittney Griner is the latest American to be detained in Russia as supporters work desperately to free them"),
+            Sport("Tennis",R.drawable.image4, summary = "Basketball star Brittney Griner is the latest American to be detained in Russia as supporters work desperately to free them"),
+            Sport("Dodge ball",R.drawable.image5, summary = "Basketball star Brittney Griner is the latest American to be detained in Russia as supporters work desperately to free them"),
+        )
+    }
+
+
+    }
---
+++ app/src/main/java/com/example/sport_news/Models/Sport.kt
@@ -0,0 +1,10 @@
+package com.example.sport_news.Models
+
+import android.media.Image
+
+data class Sport (
+    val name:String="",
+    val image: Int=0,
+    val category: String = "news",
+    val summary : String = "no summary"
+)
